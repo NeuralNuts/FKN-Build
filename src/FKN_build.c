e44386291376eba96eda5ad318e41da9763ff2af
NeuralNuts <wilsonzakk@gmail.com>
2024-03-23 01:27:54 +1000

    finished FKN_BUILD_IT_FOR_ME()

--- src/FKN_build.c
+++ src/FKN_build.c
@@ -13,17 +13,53 @@
 #define MAX_FILE_EXT 255
 #define FILE_EXT_DOT '.'
 
-char *check_file_extension(const char *file_extension, char *file_name) {
+#define TICK "✓"
+
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+struct FoundFiles {
+    char file_name[256];
+};
+
+void print_found_files(struct FoundFiles found_files[]) {
+    for(int i = 0; i < 255; i++) {
+        if(found_files[i].file_name[0] == '\0') {
+            break;
+        }
+        else {
+            printf(CYN "[BUILD_FILES] " RESET "%s\t" CYN "[OUTCOME] " GRN TICK "\n", found_files[i].file_name);
+        }
+    }
+}
+
+void insert_into_found_files(struct FoundFiles found_files[], char *file_name) {
+    for(int i = 0; i < 255; i++) {
+        if(found_files[i].file_name[0] == '\0') {
+            strcpy(found_files[i].file_name, file_name);
+
+            found_files[i].file_name[sizeof(found_files[i].file_name) - 1] = '\0';
+
+            break;
+        }
+    }
+}
+
+void check_file_extension(struct FoundFiles found_files[], const char *file_extension, char *file_name) {
     const char *ext = strrchr(file_name, FILE_EXT_DOT);
 
     if (ext != NULL && strcmp(ext + 1, file_extension) == 0) {
-        printf("[FILES_FOUND] %s\t\n", file_name);
+        insert_into_found_files(found_files, file_name);
     }
-
-    return 0;
 }
 
-char *get_build_files(const char *dir_path, const char *file_extension) {
+void get_build_files(struct FoundFiles found_files[], const char *dir_path, const char *file_extension) {
     DIR *folder;
     struct dirent *dir_entry;
 
@@ -38,11 +74,10 @@ char *get_build_files(const char *dir_path, const char *file_extension) {
         if (strcmp(dir_entry->d_name, ".") == 0 ||
             strcmp(dir_entry->d_name, "..") == 0) continue;
 
-        check_file_extension(file_extension, dir_entry->d_name);
+        check_file_extension(found_files, file_extension, dir_entry->d_name);
     }
 
     closedir(folder);
-    return 0;
 }
 
 int FKN_GET_ARRAY_LENGTH(struct BuildFiles build_files[]) {
@@ -60,6 +95,8 @@ void FKN_BUILD_IT_FOR_ME(const char *dir_path, const char *file_extension, char 
                           (FILENAME_MAX + 2) + 1;
 
     char *result = malloc(total_length);
+
+    struct FoundFiles found_files[255] = {0};
     
     if (result == NULL) {
         perror("Memory allocation failed");
@@ -69,11 +106,15 @@ void FKN_BUILD_IT_FOR_ME(const char *dir_path, const char *file_extension, char 
     strncpy(result, GCC_O, total_length);
     strncat(result, exe_name, strnlen(exe_name, MAX_EXE_NAME));
 
+    get_build_files(found_files, dir_path, file_extension);
 
-    strncat(result, " ", 2); 
-    strncat(result, get_build_files(dir_path, file_extension), total_length);
+    for(int i = 0; i < 3; i++) {
+        strncat(result, " ", 2);
+        strncat(result, dir_path, total_length);
+        strncat(result, found_files[i].file_name, total_length);
+    }
 
-    (void)printf("[BUILD_FILES] \t %s \n", get_build_files(dir_path, file_extension));
+    print_found_files(found_files);
 
     system(result);
     free(result);
@@ -98,8 +139,6 @@ void FKN_BUILD(struct BuildFiles build_files[], int array_length, char exe_name[
     strncat(result, exe_name, strnlen(exe_name, MAX_EXE_NAME));
 
     for (int i = 0; i < (size_t)array_length; i++) {
-        size_t file_count = 0;
-
         strncat(result, " ", 2); 
         strncat(result, build_files[i].file_path, total_length);
 
